f519dbbbb84e2ebbb857a826d1e41caa2ae07a62
ItalyanecXXX <anatoly.bezrukoff@gmail.com>
2024-09-16 00:53:32 +0700

    header creat

--- src/js/components/theme.js
+++ src/js/components/theme.js
@@ -1,10 +1,23 @@
 export const useTheme = () => {
   const themeTarget = document.querySelector('[data-theme]');
   const themeSwticher = document.querySelector('[data-theme-switcher]');
+
+  const themeTargetDataset = themeTarget.dataset;
+
+  const theme = localStorage.getItem('theme') || 'light';
+  themeTargetDataset.theme = theme;
+
+  if (theme === 'dark') {
+    themeSwticher.checked = true;
+  }
+
   themeSwticher.addEventListener('click', () => {
-    const themeTargetDataset = themeTarget.dataset;
-    themeTargetDataset.theme == 'light'
-      ? (themeTargetDataset.theme = 'dark')
-      : (themeTargetDataset.theme = 'light');
+    if (themeTargetDataset.theme === 'light') {
+      themeTargetDataset.theme = 'dark';
+      localStorage.setItem('theme', 'dark');
+    } else {
+      themeTargetDataset.theme = 'light';
+      localStorage.setItem('theme', 'light');
+    }
   });
 };
