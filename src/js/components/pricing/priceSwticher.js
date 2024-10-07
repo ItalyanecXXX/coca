b59a876d797d9dc3b58c3b942dc411e4e9e81537
ItalianecXXX <anatoly.bezrukoff@ya.ru>
2024-10-07 20:29:06 +0700

    Create pricing: faq section

--- src/js/components/pricing/priceSwticher.js
+++ src/js/components/pricing/priceSwticher.js
@@ -3,30 +3,55 @@ export const usePriceSwitcher = () => {
   const starterPrice = document.querySelector('[data-price="starter"]');
   const popularPrice = document.querySelector('[data-price="popular"]');
   const entriesPrice = document.querySelector('[data-price="enterprise"]');
+  const yearStarter = document.querySelector('[data-price="yearStarter"]');
+  const yearPopular = document.querySelector('[data-price="yearPopular"]');
+  const yearEnterprise = document.querySelector(
+    '[data-price="yearEnterprise"]',
+  );
   const priceList = {
     starter: {
-      default: 120,
-      witchSale: 100,
+      default: 100,
+      witchSale: 960,
     },
     popular: {
-      default: 1680,
-      witchSale: 1400,
+      default: 1400,
+      witchSale: 13440,
     },
     enterprise: {
-      default: 2520,
-      witchSale: 2100,
+      default: 2100,
+      witchSale: 20160,
+    },
+  };
+  const yearList = {
+    yearStarter: {
+      default: '/mo',
+      witchSale: '/yr.',
+    },
+    yearPopular: {
+      default: '/mo',
+      witchSale: '/yr.',
+    },
+    yearEnterprise: {
+      default: '/mo',
+      witchSale: '/yr.',
     },
   };
   const setPricesWitchSale = () => {
     starterPrice.textContent = priceList.starter.witchSale;
     popularPrice.textContent = priceList.popular.witchSale;
     entriesPrice.textContent = priceList.enterprise.witchSale;
+    yearStarter.textContent = yearList.yearStarter.witchSale;
+    yearPopular.textContent = yearList.yearPopular.witchSale;
+    yearEnterprise.textContent = yearList.yearEnterprise.witchSale;
   };
 
   const setDefaultPrices = () => {
     starterPrice.textContent = priceList.starter.default;
     popularPrice.textContent = priceList.popular.default;
     entriesPrice.textContent = priceList.enterprise.default;
+    yearStarter.textContent = yearList.yearStarter.default;
+    yearPopular.textContent = yearList.yearPopular.default;
+    yearEnterprise.textContent = yearList.yearEnterprise.default;
   };
 
   switcher.checked = true;
